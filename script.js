e113c957b11c26a194ed97c31baf1abf12a2f518
enfusq <aivars.trumars@gmail.com>
2025-09-19 13:21:14 +0300

    HTML DOM element variables made

---
+++ script.js
@@ -0,0 +1,7 @@
+const amount = document.getElementById("amount");
+const btn = document.getElementById("btn");
+const list = document.getElementById("list");
+
+btn.addEventListener("click", async () => {
+    
+})
