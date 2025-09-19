0cc3ac688c268a61feca9560bb454280cbb1d741
enfusq <aivars.trumars@gmail.com>
2025-09-19 13:28:11 +0300

    Posts fetched

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const amount = document.getElementById("amount");
 const btn = document.getElementById("btn");
 const list = document.getElementById("list");
 
-btn.addEventListener("click", async () => {
-    
-})
+async function fetchPosts() {
+    const apiUrl = `https://jsonplaceholder.typicode.com/posts?_limit=${amount.value}`;
+    const response = await fetch(apiUrl);
+    const posts = await response.json();
+}
+
+btn.addEventListener("click", fetchPosts)
